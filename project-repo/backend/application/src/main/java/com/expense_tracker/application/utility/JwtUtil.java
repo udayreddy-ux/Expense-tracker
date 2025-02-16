feaf97bfaa9d66e3864b8f18fa5899ece24f88bc
KOVVURI UDAY SURYA DEVESWAR REDDY <60887877+udayreddy-ux@users.noreply.github.com>
2025-02-16 14:39:42 -0500

    Update JwtUtil.java

--- project-repo/backend/application/src/main/java/com/expense_tracker/application/utility/JwtUtil.java
+++ project-repo/backend/application/src/main/java/com/expense_tracker/application/utility/JwtUtil.java
@@ -3,9 +3,12 @@ package com.expense_tracker.application.utility;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
+
+@Component
 public class JwtUtil {
 	
     @Value("${JWT_SECRET}")
