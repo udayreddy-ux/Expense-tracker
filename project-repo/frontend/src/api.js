28aa9a022e189e069c5a25a5a1713d577868310d
KOVVURI UDAY SURYA DEVESWAR REDDY <60887877+udayreddy-ux@users.noreply.github.com>
2025-03-30 15:29:49 -0400

    Update api.js

--- project-repo/frontend/src/api.js
+++ project-repo/frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  //baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: http://localhost:8080/api,
 });
 console.log("ENV URL:", process.env.REACT_APP_API_BASE_URL);
 
